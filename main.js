c1c321430f94303aa2f6fc74ea9342867f1b8c06
Johno Caldwell <jjcldwll@gmail.com>
2022-04-13 18:24:08 -0400

    Initital commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+}
+
+theHobbit = new Book("Hobbit", "JRR Tolkein", 297, false);
+console.log(theHobbit.info());
